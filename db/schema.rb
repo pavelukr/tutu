93f077934cd08899354a57d78a9c2c4168fca1d7
pavelukr <pavlik99tishkov@gmail.com>
2021-02-03 16:15:04 +0200

    Lesson 11

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_02_144242) do
+ActiveRecord::Schema.define(version: 2021_02_03_125726) do
 
   create_table "railway_stations", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
