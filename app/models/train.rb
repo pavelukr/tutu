797c54d5565ef3c592e82b7cde36b510fa8aa90a
pavelukr <pavlik99tishkov@gmail.com>
2021-02-04 15:26:45 +0200

    Lesson 12

--- app/models/train.rb
+++ app/models/train.rb
@@ -1,2 +1,10 @@
 class Train < ApplicationRecord
+  validates :number, presence: true
+
+  has_many :tickets
+  belongs_to :current_station, class_name: "RailwayStation", foreign_key: :current_station_id
+  belongs_to :route
 end
+#Билет содержит информацию о (связан с) поезде, начальной и конечной станциях. Билет принадлежит пользователю
+#   Пользователь может иметь произвольное кол-во билетов
+#   Поезд содержит информацию о билетах на него.
