3d493070bf4db470e3d9f542aeca633186f2fb71
pavelukr <pavlik99tishkov@gmail.com>
2021-02-22 18:18:27 +0200

    Lesson 17

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-  resources :railway_stations do
-    patch :update_position, on: :member
-    patch :update_time, on: :member
-  end
-  resources :routes
-  resources :trains do
-    resources :railway_carriages, :tickets, shallow: true
+  devise_for :users
+
+  namespace :admin do
+    resources :railway_stations do
+      patch :update_position, on: :member
+      patch :update_time, on: :member
+    end
+    resources :routes
+    resources :trains do
+      resources :railway_carriages, :tickets, shallow: true
+    end
   end
   resource :search, only: [:show, :new, :edit] do
     post '/', to: 'searches#search'
