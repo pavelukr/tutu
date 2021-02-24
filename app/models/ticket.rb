581c2e8d109239cea637f2091ae3f2230bc038c7
pavelukr <pavlik99tishkov@gmail.com>
2021-02-24 15:43:21 +0200

    Lesson 18

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,5 @@
 class Ticket < ApplicationRecord
+  validates :first_name, :last_name, :middle_name, :passport, presence: true
   belongs_to :train
   belongs_to :user
   belongs_to :begin_station, class_name: "RailwayStation", foreign_key: :begin_station_id
