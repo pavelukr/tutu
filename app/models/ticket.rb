51197e02672fcbd555f774e937da278d7b1c506f
pavelukr <pavlik99tishkov@gmail.com>
2021-02-25 10:58:54 +0200

    Lesson 19

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,4 +4,16 @@ class Ticket < ApplicationRecord
   belongs_to :user
   belongs_to :begin_station, class_name: "RailwayStation", foreign_key: :begin_station_id
   belongs_to :end_station, class_name: "RailwayStation", foreign_key: :end_station_id
+  after_create :send_info
+  before_destroy :send_notification
+
+  private
+
+  def send_info
+    TicketsMailer.buy_ticket(self.user, self).deliver_now
+  end
+
+  def send_notification
+    TicketsMailer.destroy_ticket(self.user, self).deliver_now
+  end
 end
