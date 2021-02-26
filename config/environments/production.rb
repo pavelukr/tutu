bed4001f4fd9a7a2c49026f81ecd60b0f0c54089
pavelukr <pavlik99tishkov@gmail.com>
2021-02-26 13:07:02 +0200

    SendGrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,19 +27,15 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   config.action_mailer.default_url_options = { host: "serene-spire-83709.herokuapp.com"}
-  config.action_mailer.delivery_method = :sendgrid_actionmailer
-=begin
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address   => "smtp.sendgrid.net",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password  => ENV["SENDGRID_PASSWORD"], # SMTP password is any valid API key, when user_name is "apikey".
-    :authentication => 'plain',
-    :domain => 'herokuapp.com', # your domain to identify your server when connecting
+    :user_name => 'apikey',
+    :password  => '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', # SMTP password is any valid API key, when user_name is "apikey".
+    :authentication => 'login',
+    :domain => 'serene-spire-83709.herokuapp.com', # your domain to identify your server when connecting
   }
-=end
 
 
   # Compress CSS using a preprocessor.
