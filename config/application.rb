0b1b55b4913a8f8532dc2d905d5cf2113baefa0a
pavelukr <pavlik99tishkov@gmail.com>
2021-02-26 11:18:46 +0200

    Sendgrid

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Tutu
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.i18n.default_locale = :ru
+    #config.i18n.default_locale = :ru
     config.assets.precompile += %w( application )
   end
 end
