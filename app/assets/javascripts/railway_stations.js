5c8333efb575015705db31d25d92a4100f4b1129
pavelukr <pavlik99tishkov@gmail.com>
2021-03-03 12:40:20 +0200

    Lesson 20

--- app/assets/javascripts/railway_stations.js
+++ app/assets/javascripts/railway_stations.js
@@ -1,10 +1,9 @@
-$(document).ready(function () {
-    $('a.edit_station').click(function (e) {
+$(document).ready(function() {
+    $('a.edit_station').click(function(e) {
         e.preventDefault();
-        let station_id = $(this).data('stationId');
-
-        let form = $('#edit_railway_station_' + station_id);
-        let title = $('#railway_station_title_' + station_id);
+        const station_id = $(this).data('stationId');
+        const form = $('#edit_railway_station_' + station_id);
+        const title = $('#railway_station_title_' + station_id);
 
         if (!$(this).hasClass('cancel')) {
             $(this).html('Cancel')
